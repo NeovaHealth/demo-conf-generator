75a865828b63b2af529ac7f4607ffa764374a8f9
Colin Wren <colin@neovahealth.co.uk>
2016-01-01 21:14:17 +0000

    Fixing bug where directory structure was messes up

--- configuration_generator/module_skeleton.py
+++ configuration_generator/module_skeleton.py
@@ -31,33 +31,31 @@ class ModuleSkeleton(object):
         - - - - xml
         """
         dir_str = '{0}/{1}'
-        module_folder = (dir_str.format(self.module_path, self.module_name),
-                         self.module_name)
-        data_folder = (dir_str.format(module_folder[0], 'data'), 'data')
-        static_folder = (dir_str.format(module_folder[0], 'static'), 'static')
-        src_folder = (dir_str.format(static_folder[0], 'src'), 'src')
-        img_folder = (dir_str.format(src_folder[0], 'img'), 'img')
-        xml_folder = (dir_str.format(src_folder[0], 'xml'), 'xml')
-        folders = [module_folder, data_folder, static_folder,
+        root_path = dir_str.format(self.module_path, self.module_name)
+        data_folder = dir_str.format(root_path, 'data')
+        static_folder = dir_str.format(root_path, 'static')
+        src_folder = dir_str.format(root_path, 'static/src')
+        img_folder = dir_str.format(root_path, 'static/src/img')
+        xml_folder = dir_str.format(root_path, 'static/src/xml')
+        folders = [root_path, data_folder, static_folder,
                    src_folder, img_folder, xml_folder]
         for folder in folders:
             try:
-                self.create_folder(folder[0], folder[1])
+                self.create_folder(folder)
             except OSError:
                 print 'Error encountered when creating module skeleton'
 
     @staticmethod
-    def create_folder(path, name):
+    def create_folder(path):
         """
         Create a folder in a given path with given name
         :param path: Where to put folder
         :param name: What to call folder
         """
-        folder = '{0}/{1}'.format(path, name)
         try:
-            os.makedirs(folder)
+            os.makedirs(path)
         except OSError as exc:
-            if exc.errno == errno.EEXIST and os.path.isdir(folder):
+            if exc.errno == errno.EEXIST and os.path.isdir(path):
                 pass
             else:
                 raise
