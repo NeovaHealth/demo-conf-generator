c2bf3c7dde9df06656261273ef834c0eec6111b9
Colin Wren <colin@neovahealth.co.uk>
2016-01-01 20:36:21 +0000

    Adding setup.py file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="openeobs_config_generator",
+    version="0.1",
+    description="A tool for creating configurations for Open-eObs",
+    author="Colin Wren",
+    author_email="colin@gimpneek.com",
+    url="http://github.com/NeovaHealth/demo-conf-generator",
+    provides=["openeobs_config_generator"],
+    packages=['configuration_generator'],
+    entry_points={
+        "console_scripts": [
+            "new_openeobs_config = configuration_generator.__main__:main"
+        ],
+    },
+    install_requires=['Jinja2==2.8', 'CairoSVG==1.0.19', 'argparse>=1.4.0'],
+    license="GPL",
+    zip_safe=True,
+)
