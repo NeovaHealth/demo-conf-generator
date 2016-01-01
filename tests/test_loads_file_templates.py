5bff91f635d93504d6f27f0f93fecf0780770a75
Colin Wren <colin@neovahealth.co.uk>
2016-01-01 16:29:39 +0000

    Adding Jinja2 templates for files

--- tests/test_loads_file_templates.py
+++ tests/test_loads_file_templates.py
@@ -18,6 +18,30 @@ class TestTemplateLoading(unittest.TestCase):
         """
         Test that the logo template loads
         """
-        template = self.env.get_template('logo.svg')
+        template = self.env.get_template('logo.svg.j2')
         self.assertTrue(isinstance(template, Template),
                         'Logo Template not loaded')
+
+    def test_02_loads_openerp_template(self):
+        """
+        Test that the __openerp__.py file template loads
+        """
+        template = self.env.get_template('__openerp__.py.j2')
+        self.assertTrue(isinstance(template, Template),
+                        '__openerp__.py Template not loaded')
+
+    def test_03_loads_master_data_template(self):
+        """
+        Test that the master_data.xml file template loads
+        """
+        template = self.env.get_template('master_data.xml.j2')
+        self.assertTrue(isinstance(template, Template),
+                        'master_data.xml Template not loaded')
+
+    def test_04_loads_view_override_data_template(self):
+        """
+        Test that the view_override.xml file template loads
+        """
+        template = self.env.get_template('view_override.xml.j2')
+        self.assertTrue(isinstance(template, Template),
+                        'view_override.xml Template not loaded')
