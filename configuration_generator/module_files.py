f26bbf5c627fddbf3480c122400af4e24bab2637
Colin Wren <colin@gimpneek.com>
2016-01-13 13:07:54 +0000

    Reverting back to 745 default due to cut off on short logos

--- configuration_generator/module_files.py
+++ configuration_generator/module_files.py
@@ -62,9 +62,9 @@ class ModuleFiles(object):
         logo_temp = self.env.get_template('logo.svg.j2')
         # render logo.svg
         char_width = 20
-        svg_width = 200 + (len(self.trust_name) * char_width) # 745
-        if len(self.trust_status) > len(self.trust_name):
-            svg_width = 200 + (len(self.trust_status) * char_width)
+        svg_width = 745
+        if len(self.trust_name) > 30:
+            svg_width = 200 + (len(self.trust_name) * char_width)
         logo_offset = svg_width - 200  # 545
         text_width = logo_offset - 25  # 520
         logo_file = logo_temp.render(trust_name=self.trust_name,
